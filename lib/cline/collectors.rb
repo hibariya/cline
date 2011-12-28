1f5b4d664f3b08c2aa3360dc1c9f494ff8a0d543
hibariya <celluloid.key@gmail.com>
2011-12-28 11:43:03 +0900

    fix-bug

--- lib/cline/collectors.rb
+++ lib/cline/collectors.rb
@@ -7,7 +7,7 @@ module Cline::Collectors
         message     = message.encode(Encoding::UTF_8)
         notified_at = parse_time_string_if_needed(notified_at)
 
-        return if oldest_notification.notified_at.to_time > notified_at
+        return if oldest_notification && oldest_notification.notified_at.to_time > notified_at
 
         Cline::Notification.instance_exec message, notified_at do |message, notified_at|
           create(message: message, notified_at: notified_at) unless find_by_message_and_notified_at(message, notified_at)
