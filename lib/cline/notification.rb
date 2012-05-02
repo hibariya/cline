6cd4dd8324c525ec1b83e8a9a09c469c81d9428f
hibariya <celluloid.key@gmail.com>
2012-05-02 23:10:19 +0900

    scope for id_alias

--- lib/cline/notification.rb
+++ lib/cline/notification.rb
@@ -6,8 +6,12 @@ module Cline
     validate :message, presence: true, uniqueness: true
     validate :display_count, presence: true, numerically: true
 
-    scope :with_volatile_id, -> {
-      select('(id - (SELECT MIN(id) FROM notifications)) AS volatile_id, *')
+    scope :with_id_alias, -> {
+      select('(id - (SELECT MIN(id) FROM notifications)) AS id_alias, *')
+    }
+
+    scope :by_alias_string, ->(alias_string) {
+      with_id_alias.where('id_alias = ?', alias_string.to_i(36))
     }
 
     scope :by_keyword, ->(word) {
@@ -32,7 +36,7 @@ module Cline
 
     class << self
       def display(offset = 0)
-        with_volatile_id.
+        with_id_alias.
           earliest(1, offset).
           first.
           display
@@ -58,9 +62,12 @@ module Cline
 
     def display_message
       display_time = notified_at.strftime('%Y/%m/%d %H:%M')
-      alias_id     = volatile_id.to_i.to_s(36)
 
-      "[#{display_time}][#{display_count}][$#{alias_id}] #{message}"
+      "[#{display_time}][#{display_count}][$#{id_alias_string}] #{message}"
+    end
+
+    def id_alias_string
+      id_alias.to_i.to_s(36)
     end
   end
 end
